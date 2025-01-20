aa328e091261ff194ceb0abf966bf7823746354f
mohamed <mohamed750boukhari@gmail.com>
2025-01-20 20:31:58 +0100

    important Note

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router/AppRouter";
 
+// FIXME: Redux Toolkit
+import { Provider } from "react-redux";
+import store from "@store/index";
+// FIXME: styles bootstrap
 /* The following line can be included in your src/index.js or App.js file */
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
