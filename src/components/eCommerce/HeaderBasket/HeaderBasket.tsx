5d7ef5e067e997b60e74ec5085752fcf6af5e515
mohamed <mohamed750boukhari@gmail.com>
2025-01-22 20:54:50 +0100

    show Total Quantity with (Biggners Approach)

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,12 +1,18 @@
+import { useAppSelector } from "@store/hooks";
+import { getCartTotalQuantity } from "@store/cart/cartSlice";
+
 import Logo from "@assets/svg/cart.svg?react";
 import style from "./style.module.css";
 
 const { basketContainer, basketQuantity } = style;
+
 const HeaderBasket = () => {
+  const cartItems = useAppSelector(getCartTotalQuantity);
+
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" />
-      <div className={basketQuantity}></div>
+      <div className={basketQuantity}>{cartItems}</div>
     </div>
   );
 };
--- src/store/cart/cartSlice.ts
+++ src/store/cart/cartSlice.ts
@@ -27,5 +27,19 @@ const cartSlice = createSlice({
   },
 });
 
+const getCartTotalQuantity = (state) => {
+  console.log("function")
+  const totalQuantity = Object.values(state.cart.items).reduce(
+    (accumulator, currentValue) => {
+      return accumulator + currentValue;
+    },
+    0
+  );
+  return totalQuantity;
+};
+
+
+
+export { getCartTotalQuantity }
 export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
