9355882f57b38960bb0143116d5446ab584511c3
mohamed <mohamed750boukhari@gmail.com>
2025-01-23 23:44:18 +0100

    Debouncing Add to Cart Action

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "@store/hooks";
 import { addToCart } from "@store/cart/cartSlice";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import { TProduct } from "@customTypes/product";
+
 import styles from "./style.module.css";
 
 const { product, productImg } = styles;
@@ -9,9 +11,26 @@ const { product, productImg } = styles;
 const Product = ({ title, price, img, id }: TProduct) => {
   const dispatch = useAppDispatch();
 
+  const [isBTNClicked, setIsBTNClicked] = useState(0);
+  const [isBTNDisabled, setIsBTNDisabled] = useState(false);
+
+  useEffect(() => {
+    if (!isBTNClicked) {
+      return;
+    }
+    setIsBTNDisabled(true);
+    const debounce = setTimeout(() => {
+      setIsBTNDisabled(false);
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [isBTNClicked]);
+
   const addToCartHandler = () => {
     dispatch(addToCart(id));
+    setIsBTNClicked((prev) => prev + 1);
   };
+
   return (
     <div className={product}>
       <div className={productImg}>
@@ -20,11 +39,18 @@ const Product = ({ title, price, img, id }: TProduct) => {
       <h2 title={title}>{title}</h2>
       <h3>{price}</h3>
       <Button
-        onClick={ addToCartHandler}
+        onClick={addToCartHandler}
         variant="info"
         style={{ color: "white" }}
+        disabled={isBTNDisabled}
       >
-        Add to cart
+        {isBTNDisabled ? (
+          <>
+            <Spinner animation="border" size="sm" /> Loading...
+          </>
+        ) : (
+          "Add to cart"
+        )}
       </Button>
     </div>
   );
