db604c37f91f7c995669f530a9f820d310f38ca1
mohamed <mohamed750boukhari@gmail.com>
2025-01-23 22:59:45 +0100

    Redux Persist non-serializable Alert

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,19 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import categories from "./categories/categoriesSlice";
 import products from "./products/productsSlice";
 import cart from "./cart/cartSlice";
-
-const rootPersistConfig = {
-  key: "root",
-  storage,
-  whiteList: ["cart"],
-  // blacklist: ["cart"]
-};
 
 const cartPersistConfig = {
   key: "cart",
@@ -24,10 +26,15 @@ const rootReducer = combineReducers({
   products,
   cart: persistReducer(cartPersistConfig, cart),
 });
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
