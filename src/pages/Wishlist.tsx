a1ffe9927dd51d796d16aa86e340a151bb24f02c
mohamed <mohamed750boukhari@gmail.com>
2025-01-27 21:27:47 +0100

    render Wishlist

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,9 +1,38 @@
-import { Heading } from "@components/common";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+
+import { Product } from "@components/eCommerce";
+import { GridList, Heading } from "@components/common";
+import { TProduct } from "@customTypes/product";
+import { actGetWishlist } from "@store/wishlist/wishlistSlice";
+import { Loading } from "@components/feedback";
 
 const Wishlist = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error, productsFullInfo } = useAppSelector(
+    (state) => state.wishlist
+  );
+  const cartItems = useAppSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    dispatch(actGetWishlist());
+  }, [dispatch]);
+
+  const records = productsFullInfo.map((el) => ({
+    ...el,
+    quantity: cartItems[el.id],
+    isLiked: true,
+  }));
+
   return (
     <>
       <Heading>Your Wishlist</Heading>
+      <Loading status={loading} error={error}>
+        <GridList<TProduct>
+          records={records}
+          renderItem={(record) => <Product {...record} />}
+        />
+      </Loading>
     </>
   );
 };
--- src/store/wishlist/act/actGetWishlist.ts
+++ src/store/wishlist/act/actGetWishlist.ts
@@ -8,20 +8,24 @@ const actGetWishlist = createAsyncThunk(
   "wishlist/actGetWishlist",
   async (_, thunkAPI) => {
     const { rejectWithValue, fulfillWithValue } = thunkAPI;
+
     try {
       const userWishlist = await axios.get<{ productId: number }[]>(
-        "wishlist?userId=1"
+        "/wishlist?userId=1"
       );
+
       if (!userWishlist.data.length) {
         return fulfillWithValue([]);
       }
 
       const concatenatedItemsId = userWishlist.data
-        .map((el) => `id${el.productId}`)
+        .map((el) => `id=${el.productId}`)
         .join("&");
 
-      const response = axios.get<TResponse>(`/products?${concatenatedItemsId}`);
-      return response;
+      const response = await axios.get<TResponse>(
+        `/products?${concatenatedItemsId}`
+      );
+      return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
         return rejectWithValue(error.response?.data.message || error.message);
@@ -31,4 +35,5 @@ const actGetWishlist = createAsyncThunk(
     }
   }
 );
+
 export default actGetWishlist;
