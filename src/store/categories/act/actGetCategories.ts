6f06e493692620ec425e8401beec4c4025545f90
mohamed <mohamed750boukhari@gmail.com>
2025-01-29 23:33:42 +0100

    git Utils for reusable functions

--- src/store/categories/act/actGetCategories.ts
+++ src/store/categories/act/actGetCategories.ts
@@ -1,24 +1,21 @@
 import { TCategory } from "@customTypes/category";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosErrorHandler } from "src/utils";
 import axios from "axios";
 
 type TResponse = TCategory[];
 
 const actGetCategories = createAsyncThunk(
-    "categories/actGetCategories",
-    async (_, thunkAPI) => {
-        const { rejectWithValue } = thunkAPI;
-        try {
-            const response = await axios.get<TResponse>("/categories");
-            return response.data;
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                return rejectWithValue(error.response?.data.message);
-            } else {
-                return rejectWithValue("An error occurred while fetching categories.");
-            }
-        }
+  "categories/actGetCategories",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const response = await axios.get<TResponse>("/categories");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(axiosErrorHandler(error));
     }
+  }
 );
 
 export default actGetCategories;
--- src/store/wishlist/act/actLikeToggle.ts
+++ src/store/wishlist/act/actLikeToggle.ts
@@ -1,29 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosErrorHandler } from "src/utils";
 import axios from "axios";
 
 const actLikeToggle = createAsyncThunk(
-    "wishlist/actLikeToggle",
-    async (id: number, thunkAPI) => {
-        const { rejectWithValue } = thunkAPI;
-        try {
-            const idRecordExist = await axios.get(
-                `/wishlist?userId=1&productId=${id}`
-            );
-            if (idRecordExist.data.length) {
-                await axios.delete(`/wishlist/${idRecordExist.data[0].id}`);
-                return { type: "remove", id };
-            } else {
-                await axios.post("/wishlist", { userId: 1, productId: id });
-                return { type: "add", id };
-            }
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                return rejectWithValue(error.response?.data.message || error.message);
-            } else {
-                return rejectWithValue("An unexpected error");
-            }
-        }
+  "wishlist/actLikeToggle",
+  async (id: number, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const idRecordExist = await axios.get(
+        `/wishlist?userId=1&productId=${id}`
+      );
+      if (idRecordExist.data.length) {
+        await axios.delete(`/wishlist/${idRecordExist.data[0].id}`);
+        return { type: "remove", id };
+      } else {
+        await axios.post("/wishlist", { userId: 1, productId: id });
+        return { type: "add", id };
+      }
+    } catch (error) {
+      return rejectWithValue(axiosErrorHandler(error));
     }
+  }
 );
 
 export default actLikeToggle;
