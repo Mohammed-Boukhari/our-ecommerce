51f564204c91213f8533c6e642a3bfef2f799589
mohamed <mohamed750boukhari@gmail.com>
2025-03-10 18:40:12 +0100

    Apply progress baar and auto close toast v2.0.0

--- src/components/feedback/toast/ToastItem.tsx
+++ src/components/feedback/toast/ToastItem.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from "react";
 import { useAppDispatch } from "@store/hooks";
 import { removeToast } from "@store/toasts/toastsSlice";
 
@@ -9,6 +10,36 @@ type TTostItemProps = TToast;
 
 const ToastItem = ({ message, type, title, id }: TTostItemProps) => {
   const dispatch = useAppDispatch();
+  const [progressBarINdicator, setProgressBarINdicator] = useState(0);
+
+  const progressBarScale = 100;
+  const duration = 4000;
+  const intervalTime = duration / 100;
+
+  const closeToastHandler = useCallback(() => {
+    dispatch(removeToast(id));
+  }, [dispatch, id]);
+
+  // TODO: progress bar calculate
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setProgressBarINdicator((prevState) => {
+        if (prevState < progressBarScale) {
+          return prevState + 1;
+        }
+        return prevState;
+      });
+
+      return () => clearInterval(timerId);
+    }, intervalTime);
+  }, [intervalTime]);
+
+  // TODO: close toast when progress bar 100%
+  useEffect(() => {
+    if (progressBarINdicator === 100) {
+      closeToastHandler();
+    }
+  }, [progressBarINdicator, closeToastHandler]);
 
   return (
     <div className={`alert alert-${type} ${toastItem}`}>
@@ -16,9 +47,15 @@ const ToastItem = ({ message, type, title, id }: TTostItemProps) => {
       <p>{message}</p>
       <button
         className="btn-close"
-        onClick={() => dispatch(removeToast(id))}
+        onClick={() => closeToastHandler()}
       ></button>
-      <span className=" placeholder" style={{ width: "100%" }}></span>
+      <span
+        className=" placeholder"
+        style={{
+          width: `${progressBarINdicator}%`,
+          transition: `width ${intervalTime}ms liner`,
+        }}
+      ></span>
     </div>
   );
 };
